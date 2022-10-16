cf769b5a64e106893b728e48ba4a61f3d6f10c32
petitpissenlit <101569407+petitpissenlit@users.noreply.github.com>
2022-10-16 22:17:57 +0200

    Add files via upload

---
+++ Oefening3/Oefening3/Form1.Designer.cs
@@ -0,0 +1,409 @@
+﻿namespace Oefening3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbint = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tblft = new System.Windows.Forms.TextBox();
+            this.tbprsn = new System.Windows.Forms.TextBox();
+            this.tbstring = new System.Windows.Forms.TextBox();
+            this.btntoe1 = new System.Windows.Forms.Button();
+            this.btnver1 = new System.Windows.Forms.Button();
+            this.btnleeg1 = new System.Windows.Forms.Button();
+            this.btnshow1 = new System.Windows.Forms.Button();
+            this.btnsearch1 = new System.Windows.Forms.Button();
+            this.btncopy1 = new System.Windows.Forms.Button();
+            this.lbdepo = new System.Windows.Forms.Label();
+            this.lbwithdraw = new System.Windows.Forms.Label();
+            this.lbadr = new System.Windows.Forms.Label();
+            this.lbinfo = new System.Windows.Forms.Label();
+            this.lbage = new System.Windows.Forms.Label();
+            this.lbdepolijst = new System.Windows.Forms.Label();
+            this.lbwithlijst = new System.Windows.Forms.Label();
+            this.lbadreslijst = new System.Windows.Forms.Label();
+            this.btntoe2 = new System.Windows.Forms.Button();
+            this.btnver2 = new System.Windows.Forms.Button();
+            this.btnleeg2 = new System.Windows.Forms.Button();
+            this.btnshow2 = new System.Windows.Forms.Button();
+            this.btnsearch2 = new System.Windows.Forms.Button();
+            this.btncopy2 = new System.Windows.Forms.Button();
+            this.btnver3 = new System.Windows.Forms.Button();
+            this.btnleeg3 = new System.Windows.Forms.Button();
+            this.btnshow3 = new System.Windows.Forms.Button();
+            this.btnsearch3 = new System.Windows.Forms.Button();
+            this.btncopy3 = new System.Windows.Forms.Button();
+            this.btntoe3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbint
+            // 
+            this.tbint.Location = new System.Drawing.Point(87, 70);
+            this.tbint.Name = "tbint";
+            this.tbint.Size = new System.Drawing.Size(100, 22);
+            this.tbint.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // tblft
+            // 
+            this.tblft.Location = new System.Drawing.Point(611, 70);
+            this.tblft.Name = "tblft";
+            this.tblft.Size = new System.Drawing.Size(100, 22);
+            this.tblft.TabIndex = 3;
+            // 
+            // tbprsn
+            // 
+            this.tbprsn.Location = new System.Drawing.Point(416, 70);
+            this.tbprsn.Name = "tbprsn";
+            this.tbprsn.Size = new System.Drawing.Size(100, 22);
+            this.tbprsn.TabIndex = 4;
+            // 
+            // tbstring
+            // 
+            this.tbstring.Location = new System.Drawing.Point(253, 70);
+            this.tbstring.Name = "tbstring";
+            this.tbstring.Size = new System.Drawing.Size(100, 22);
+            this.tbstring.TabIndex = 5;
+            // 
+            // btntoe1
+            // 
+            this.btntoe1.Location = new System.Drawing.Point(70, 135);
+            this.btntoe1.Name = "btntoe1";
+            this.btntoe1.Size = new System.Drawing.Size(128, 23);
+            this.btntoe1.TabIndex = 6;
+            this.btntoe1.Text = "Toevoegen";
+            this.btntoe1.UseVisualStyleBackColor = true;
+            // 
+            // btnver1
+            // 
+            this.btnver1.Location = new System.Drawing.Point(70, 164);
+            this.btnver1.Name = "btnver1";
+            this.btnver1.Size = new System.Drawing.Size(128, 23);
+            this.btnver1.TabIndex = 7;
+            this.btnver1.Text = "Verwijderen";
+            this.btnver1.UseVisualStyleBackColor = true;
+            // 
+            // btnleeg1
+            // 
+            this.btnleeg1.Location = new System.Drawing.Point(70, 193);
+            this.btnleeg1.Name = "btnleeg1";
+            this.btnleeg1.Size = new System.Drawing.Size(128, 23);
+            this.btnleeg1.TabIndex = 8;
+            this.btnleeg1.Text = "Leeg maken";
+            this.btnleeg1.UseVisualStyleBackColor = true;
+            // 
+            // btnshow1
+            // 
+            this.btnshow1.Location = new System.Drawing.Point(70, 222);
+            this.btnshow1.Name = "btnshow1";
+            this.btnshow1.Size = new System.Drawing.Size(128, 23);
+            this.btnshow1.TabIndex = 9;
+            this.btnshow1.Text = "show list";
+            this.btnshow1.UseVisualStyleBackColor = true;
+            // 
+            // btnsearch1
+            // 
+            this.btnsearch1.Location = new System.Drawing.Point(70, 251);
+            this.btnsearch1.Name = "btnsearch1";
+            this.btnsearch1.Size = new System.Drawing.Size(128, 23);
+            this.btnsearch1.TabIndex = 10;
+            this.btnsearch1.Text = "search";
+            this.btnsearch1.UseVisualStyleBackColor = true;
+            // 
+            // btncopy1
+            // 
+            this.btncopy1.Location = new System.Drawing.Point(70, 280);
+            this.btncopy1.Name = "btncopy1";
+            this.btncopy1.Size = new System.Drawing.Size(128, 23);
+            this.btncopy1.TabIndex = 11;
+            this.btncopy1.Text = "copy";
+            this.btncopy1.UseVisualStyleBackColor = true;
+            // 
+            // lbdepo
+            // 
+            this.lbdepo.AutoSize = true;
+            this.lbdepo.Location = new System.Drawing.Point(111, 42);
+            this.lbdepo.Name = "lbdepo";
+            this.lbdepo.Size = new System.Drawing.Size(54, 16);
+            this.lbdepo.TabIndex = 24;
+            this.lbdepo.Text = "Deposit";
+            // 
+            // lbwithdraw
+            // 
+            this.lbwithdraw.AutoSize = true;
+            this.lbwithdraw.Location = new System.Drawing.Point(273, 42);
+            this.lbwithdraw.Name = "lbwithdraw";
+            this.lbwithdraw.Size = new System.Drawing.Size(62, 16);
+            this.lbwithdraw.TabIndex = 25;
+            this.lbwithdraw.Text = "Withdraw";
+            // 
+            // lbadr
+            // 
+            this.lbadr.AutoSize = true;
+            this.lbadr.Location = new System.Drawing.Point(444, 42);
+            this.lbadr.Name = "lbadr";
+            this.lbadr.Size = new System.Drawing.Size(50, 16);
+            this.lbadr.TabIndex = 26;
+            this.lbadr.Text = "Adress";
+            // 
+            // lbinfo
+            // 
+            this.lbinfo.AutoSize = true;
+            this.lbinfo.Location = new System.Drawing.Point(488, 9);
+            this.lbinfo.Name = "lbinfo";
+            this.lbinfo.Size = new System.Drawing.Size(28, 16);
+            this.lbinfo.TabIndex = 27;
+            this.lbinfo.Text = "Info";
+            // 
+            // lbage
+            // 
+            this.lbage.AutoSize = true;
+            this.lbage.Location = new System.Drawing.Point(641, 42);
+            this.lbage.Name = "lbage";
+            this.lbage.Size = new System.Drawing.Size(32, 16);
+            this.lbage.TabIndex = 28;
+            this.lbage.Text = "Age";
+            // 
+            // lbdepolijst
+            // 
+            this.lbdepolijst.AutoSize = true;
+            this.lbdepolijst.Location = new System.Drawing.Point(67, 323);
+            this.lbdepolijst.Name = "lbdepolijst";
+            this.lbdepolijst.Size = new System.Drawing.Size(73, 16);
+            this.lbdepolijst.TabIndex = 29;
+            this.lbdepolijst.Text = "Depositlijst";
+            // 
+            // lbwithlijst
+            // 
+            this.lbwithlijst.AutoSize = true;
+            this.lbwithlijst.Location = new System.Drawing.Point(67, 359);
+            this.lbwithlijst.Name = "lbwithlijst";
+            this.lbwithlijst.Size = new System.Drawing.Size(77, 16);
+            this.lbwithlijst.TabIndex = 30;
+            this.lbwithlijst.Text = "withdrawlijst";
+            // 
+            // lbadreslijst
+            // 
+            this.lbadreslijst.AutoSize = true;
+            this.lbadreslijst.Location = new System.Drawing.Point(67, 398);
+            this.lbadreslijst.Name = "lbadreslijst";
+            this.lbadreslijst.Size = new System.Drawing.Size(62, 16);
+            this.lbadreslijst.TabIndex = 31;
+            this.lbadreslijst.Text = "Adreslijst";
+            // 
+            // btntoe2
+            // 
+            this.btntoe2.Location = new System.Drawing.Point(238, 135);
+            this.btntoe2.Name = "btntoe2";
+            this.btntoe2.Size = new System.Drawing.Size(128, 23);
+            this.btntoe2.TabIndex = 32;
+            this.btntoe2.Text = "Toevoegen";
+            this.btntoe2.UseVisualStyleBackColor = true;
+            // 
+            // btnver2
+            // 
+            this.btnver2.Location = new System.Drawing.Point(238, 164);
+            this.btnver2.Name = "btnver2";
+            this.btnver2.Size = new System.Drawing.Size(128, 23);
+            this.btnver2.TabIndex = 33;
+            this.btnver2.Text = "Verwijderen";
+            this.btnver2.UseVisualStyleBackColor = true;
+            // 
+            // btnleeg2
+            // 
+            this.btnleeg2.Location = new System.Drawing.Point(238, 193);
+            this.btnleeg2.Name = "btnleeg2";
+            this.btnleeg2.Size = new System.Drawing.Size(128, 23);
+            this.btnleeg2.TabIndex = 34;
+            this.btnleeg2.Text = "Leeg maken";
+            this.btnleeg2.UseVisualStyleBackColor = true;
+            // 
+            // btnshow2
+            // 
+            this.btnshow2.Location = new System.Drawing.Point(238, 222);
+            this.btnshow2.Name = "btnshow2";
+            this.btnshow2.Size = new System.Drawing.Size(128, 23);
+            this.btnshow2.TabIndex = 35;
+            this.btnshow2.Text = "show list";
+            this.btnshow2.UseVisualStyleBackColor = true;
+            // 
+            // btnsearch2
+            // 
+            this.btnsearch2.Location = new System.Drawing.Point(238, 251);
+            this.btnsearch2.Name = "btnsearch2";
+            this.btnsearch2.Size = new System.Drawing.Size(128, 23);
+            this.btnsearch2.TabIndex = 36;
+            this.btnsearch2.Text = "search";
+            this.btnsearch2.UseVisualStyleBackColor = true;
+            // 
+            // btncopy2
+            // 
+            this.btncopy2.Location = new System.Drawing.Point(238, 280);
+            this.btncopy2.Name = "btncopy2";
+            this.btncopy2.Size = new System.Drawing.Size(128, 23);
+            this.btncopy2.TabIndex = 37;
+            this.btncopy2.Text = "copy";
+            this.btncopy2.UseVisualStyleBackColor = true;
+            // 
+            // btnver3
+            // 
+            this.btnver3.Location = new System.Drawing.Point(405, 164);
+            this.btnver3.Name = "btnver3";
+            this.btnver3.Size = new System.Drawing.Size(128, 23);
+            this.btnver3.TabIndex = 39;
+            this.btnver3.Text = "Verwijderen";
+            this.btnver3.UseVisualStyleBackColor = true;
+            // 
+            // btnleeg3
+            // 
+            this.btnleeg3.Location = new System.Drawing.Point(405, 193);
+            this.btnleeg3.Name = "btnleeg3";
+            this.btnleeg3.Size = new System.Drawing.Size(128, 23);
+            this.btnleeg3.TabIndex = 40;
+            this.btnleeg3.Text = "Leeg maken";
+            this.btnleeg3.UseVisualStyleBackColor = true;
+            // 
+            // btnshow3
+            // 
+            this.btnshow3.Location = new System.Drawing.Point(405, 222);
+            this.btnshow3.Name = "btnshow3";
+            this.btnshow3.Size = new System.Drawing.Size(128, 23);
+            this.btnshow3.TabIndex = 41;
+            this.btnshow3.Text = "show list";
+            this.btnshow3.UseVisualStyleBackColor = true;
+            // 
+            // btnsearch3
+            // 
+            this.btnsearch3.Location = new System.Drawing.Point(405, 251);
+            this.btnsearch3.Name = "btnsearch3";
+            this.btnsearch3.Size = new System.Drawing.Size(128, 23);
+            this.btnsearch3.TabIndex = 42;
+            this.btnsearch3.Text = "search";
+            this.btnsearch3.UseVisualStyleBackColor = true;
+            // 
+            // btncopy3
+            // 
+            this.btncopy3.Location = new System.Drawing.Point(405, 280);
+            this.btncopy3.Name = "btncopy3";
+            this.btncopy3.Size = new System.Drawing.Size(128, 23);
+            this.btncopy3.TabIndex = 43;
+            this.btncopy3.Text = "copy";
+            this.btncopy3.UseVisualStyleBackColor = true;
+            // 
+            // btntoe3
+            // 
+            this.btntoe3.Location = new System.Drawing.Point(405, 135);
+            this.btntoe3.Name = "btntoe3";
+            this.btntoe3.Size = new System.Drawing.Size(128, 23);
+            this.btntoe3.TabIndex = 38;
+            this.btntoe3.Text = "Toevoegen";
+            this.btntoe3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btncopy3);
+            this.Controls.Add(this.btnsearch3);
+            this.Controls.Add(this.btnshow3);
+            this.Controls.Add(this.btnleeg3);
+            this.Controls.Add(this.btnver3);
+            this.Controls.Add(this.btntoe3);
+            this.Controls.Add(this.btncopy2);
+            this.Controls.Add(this.btnsearch2);
+            this.Controls.Add(this.btnshow2);
+            this.Controls.Add(this.btnleeg2);
+            this.Controls.Add(this.btnver2);
+            this.Controls.Add(this.btntoe2);
+            this.Controls.Add(this.lbadreslijst);
+            this.Controls.Add(this.lbwithlijst);
+            this.Controls.Add(this.lbdepolijst);
+            this.Controls.Add(this.lbage);
+            this.Controls.Add(this.lbinfo);
+            this.Controls.Add(this.lbadr);
+            this.Controls.Add(this.lbwithdraw);
+            this.Controls.Add(this.lbdepo);
+            this.Controls.Add(this.btncopy1);
+            this.Controls.Add(this.btnsearch1);
+            this.Controls.Add(this.btnshow1);
+            this.Controls.Add(this.btnleeg1);
+            this.Controls.Add(this.btnver1);
+            this.Controls.Add(this.btntoe1);
+            this.Controls.Add(this.tbstring);
+            this.Controls.Add(this.tbprsn);
+            this.Controls.Add(this.tblft);
+            this.Controls.Add(this.tbint);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbint;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox tblft;
+        private System.Windows.Forms.TextBox tbprsn;
+        private System.Windows.Forms.TextBox tbstring;
+        private System.Windows.Forms.Button btntoe1;
+        private System.Windows.Forms.Button btnver1;
+        private System.Windows.Forms.Button btnleeg1;
+        private System.Windows.Forms.Button btnshow1;
+        private System.Windows.Forms.Button btnsearch1;
+        private System.Windows.Forms.Button btncopy1;
+        private System.Windows.Forms.Label lbdepo;
+        private System.Windows.Forms.Label lbwithdraw;
+        private System.Windows.Forms.Label lbadr;
+        private System.Windows.Forms.Label lbinfo;
+        private System.Windows.Forms.Label lbage;
+        private System.Windows.Forms.Label lbdepolijst;
+        private System.Windows.Forms.Label lbwithlijst;
+        private System.Windows.Forms.Label lbadreslijst;
+        private System.Windows.Forms.Button btntoe2;
+        private System.Windows.Forms.Button btnver2;
+        private System.Windows.Forms.Button btnleeg2;
+        private System.Windows.Forms.Button btnshow2;
+        private System.Windows.Forms.Button btnsearch2;
+        private System.Windows.Forms.Button btncopy2;
+        private System.Windows.Forms.Button btnver3;
+        private System.Windows.Forms.Button btnleeg3;
+        private System.Windows.Forms.Button btnshow3;
+        private System.Windows.Forms.Button btnsearch3;
+        private System.Windows.Forms.Button btncopy3;
+        private System.Windows.Forms.Button btntoe3;
+    }
+}
---
+++ Oefening3/Oefening3/stapel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Oef1
+{
+    internal class Stapel<T>
+    {
+
+        private List<T> Stapellijst = new List<T>();
+        private List<T> stapellijstCopy = new List<T>();
+
+
+        public void toevoegenStapel(T toeTeVoegen)
+        {
+            Stapellijst.Add(toeTeVoegen);
+        }
+
+        public T AddinStapel()
+        {
+            if (Stapellijst.Count <= 0)
+            {
+                throw new ArgumentNullException();
+            }
+
+            T updatedLijst = Stapellijst[Stapellijst.Count - 1];
+            Stapellijst.RemoveAt(Stapellijst.Count - 1);
+
+            return updatedLijst;
+        }
+
+        public void StapelLeegMaken()
+        {
+            Stapellijst.Clear();
+        }
+
+
+        public bool IsAanwezigOpStapel(T tezoeken)
+        {
+            foreach (T elements in Stapellijst)
+            {
+
+                if (elements.ToString() == tezoeken.ToString())
+                {
+
+                    return true;
+
+                }
+
+            }
+
+            return false;
+
+        }
+
+        public override string ToString()
+        {
+            string lijst = "";
+
+            foreach (T t in Stapellijst)
+            {
+                lijst += t.ToString() + " " + ",";
+            }
+
+            return lijst;
+        }
+
+        public List<T> lijstCopy()
+        {
+            stapellijstCopy = Stapellijst.GetRange(0, Stapellijst.Count);
+
+            return stapellijstCopy;
+        }
+
+    }
+}
